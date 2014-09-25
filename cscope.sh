098fb05be4f5b7b7281bdb9641366bc6175b33d5
Chip Overclock <coverclock@diag.com>
2014-09-24 20:33:22 -0600

    Minor tweaks to eliminate redundant or incorrect include directories. Still seems to be happening.

--- cscope.sh
+++ cscope.sh
@@ -2,72 +2,73 @@
 # Copyright 2014 by the Digital Aggregates Corporation, Colorado, USA.
 # Licensed under the GPLv2.
 
-LST=.cscope.lst
-REF=.cscope.out
-INF=.cscope.out.in
-POF=.cscope.out.po
+LST=".cscope.lst"
+REF=".cscope.out"
+INF=".cscope.out.in"
+POF=".cscope.out.po"
 
-if [ -f ${INF} -a -f ${POF} ]; then
-	exec cscope -qR -i${LST} -f${REF}
+if [ ! \( -f ${INF} -a -f ${POF} \) ]; then
+
+	if [ $# -eq 0 ]; then
+		DIR="."
+	else
+		DIR="$*"
+	fi
+
+	INC="`${CROSS_COMPILE}gcc -xc -E -v - < /dev/null 2>&1 | grep '^[ ]' | sed 's/^ //' | grep -v ' '`"
+
+	rm -f .cscope.*
+
+	cp /dev/null ${LST}
+
+	(
+		cd ${DIR}
+		for II in ${INC}; do
+			find -P ${II} -type f -print
+		done
+		for DD in .; do
+			find -P ${DD} \
+				-type d -name .svn -prune -o \
+				-type d -name .git -prune -o \
+				-type d -name .repo -prune -o \
+				-type d -path ./out -prune -o \
+				-type d -path ./prebuilts -prune -o \
+				-type f -a \( \
+					-name '*.[CcHhSs]' -o \
+					-name '*.[a-z]mk' -o \
+					-name '*.cc' -o \
+					-name '*.cpp' -o \
+					-name '*.cxx' -o \
+					-name '*.dts' -o \
+					-name '*.hpp' -o \
+					-name '*.hx' -o \
+					-name '*.hxx' -o \
+					-name '*.inl' -o \
+					-name '*.ld' -o \
+					-name '*.map' -o \
+					-name '*.mk' -o \
+					-name '*.rc' -o \
+					-name '*.sh' -o \
+					-name '*.xml' -o \
+					-name '*.xsd' \
+					-name '.config' -o \
+					-name 'Config.in*' -o \
+					-name 'GNUmakefile*' -o \
+					-name 'Kconfig*' -o \
+					-name '[Mm]akefile*' -o \
+					-name 'configure' -o \
+					-name '*.conf' -o \
+					-name '*.inc' -o \
+					-name '*.java' -o \
+					-name '*.x' -o \
+					-name '*.x[nru]' -o \
+					-name '*.xbn' -o \
+					-false \
+				\) -print
+		done
+	) | sed 's/^/"/;s/$/"/' > ${LST}
+
 fi
 
-rm -f .cscope.*
-cp /dev/null ${LST}
-
-if [ $# -eq 0 ]; then
-	DIR="."
-else
-	DIR="$*"
-fi
-
-INC="`${CROSS_COMPILE}gcc -xc -E -v - < /dev/null 2>&1 | grep '^[ ]' | sed 's/^ //' | grep -v ' '`"
-
-(
-	cd ${DIR}
-	for II in ${INC}; do
-		find -P ${II} -type f -print
-	done
-	for DD in .; do
-		find -P ${DD} \
-			-type d -name .svn -prune -o \
-			-type d -name .git -prune -o \
-			-type d -name .repo -prune -o \
-			-type d -path ./out -prune -o \
-			-type d -path ./prebuilts -prune -o \
-			-type f -a \( \
-				-name '*.[CcHhSs]' -o \
-				-name '*.[a-z]mk' -o \
-				-name '*.cc' -o \
-				-name '*.cpp' -o \
-				-name '*.cxx' -o \
-				-name '*.dts' -o \
-				-name '*.hpp' -o \
-				-name '*.hx' -o \
-				-name '*.hxx' -o \
-				-name '*.inl' -o \
-				-name '*.ld' -o \
-				-name '*.map' -o \
-				-name '*.mk' -o \
-				-name '*.rc' -o \
-				-name '*.sh' -o \
-				-name '*.xml' -o \
-				-name '*.xsd' \
-				-name '.config' -o \
-				-name 'Config.in*' -o \
-				-name 'GNUmakefile*' -o \
-				-name 'Kconfig*' -o \
-				-name '[Mm]akefile*' -o \
-				-name 'configure' -o \
-			    -name '*.conf' -o \
-			    -name '*.inc' -o \
-			    -name '*.java' -o \
-			    -name '*.x' -o \
-			    -name '*.x[nru]' -o \
-			    -name '*.xbn' -o \
-			    -false \
-			\) -print
-	done
-) | sed 's/^/"/;s/$/"/' > ${LST}
-
-exec cscope -qR -i${LST} -f${REF}
+exec cscope -kqR -i${LST} -f${REF}
 
