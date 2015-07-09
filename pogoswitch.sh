2dbf8e06c061d1ded74f1ce12bd05d69d7dd539a
Chip Overclock <coverclock@diag.com>
2015-07-09 16:50:33 -0600

    Iteration.

--- pogoswitch.sh
+++ pogoswitch.sh
@@ -3,19 +3,22 @@
 # Copyright 2015 Digital Aggregates Corporation, Colorado USA.
 # Licensed under the terms of the GPLv2.
 
+SERIALPORT="/dev/ttyUSB0"
+BAUDRATE="9600"
+TIMEOUT="1"
+ITERATIONS="10"
+LOGIN="admin"
+PASSWD="admin"
+OUTLET="1"
+OFFTIME="20"
+ONTIME="90"
+
 RC=0
 ZERO=`basename ${0}`
 EXPECTFILE=`mktemp /tmp/${ZERO}.XXXXXXXX`
 trap "rm -f ${EXPECTFILE}" ERR HUP INT TERM
-BAUDRATE="9600"
-SERIALPORT="/dev/ttyUSB0"
-TIMEOUT="1"
-REPEAT=10
-NETBOOTERLOGIN="admin"
-NETBOOTERPASSWD="admin"
-OUTLET="1"
 
-cat << EOF > ${EXPECTFILE}
+cat <<- EOF > ${EXPECTFILE}
 stty ospeed ${BAUDRATE} ispeed ${BAUDRATE} cs8 < ${SERIALPORT}
 set timeout ${TIMEOUT}
 spawn -open [open ${SERIALPORT} RDWR]
@@ -23,52 +26,58 @@ send "\r"
 expect { "\r>" { } timeout { abort } }
 send "login\r"
 expect { "login\rUser ID: " { } timeout { abort } }
-send "${NETBOOTERLOGIN}\r"
-expect { "${NETBOOTERLOGIN}\rPassword:" { } timeout { abort } }
-send "${NETBOOTERPASSWD}\r"
+send "${LOGIN}\r"
+expect { "${LOGIN}\rPassword:" { } timeout { abort } }
+send "${PASSWD}\r"
 expect { "*****\r>" { } timeout { abort } }
 send "pset ${OUTLET} 1\r"
 expect { "pset ${OUTPUT} 1\r>" { } timeout { abort } }
-send "logout\r"
-expect { "logout\r>" { } timeout { abort } }
 EOF
 
 expect -f ${EXPECTFILE} || exit 2
 
-cat << EOF > ${EXPECTFILE}
+sleep ${ONTIME}
+
+while [ ${ITERATIONS} -gt 0 ]; do
+
+	cat <<- EOF > ${EXPECTFILE}
+	stty ospeed ${BAUDRATE} ispeed ${BAUDRATE} cs8 < ${SERIALPORT}
+	set timeout ${TIMEOUT}
+	spawn -open [open ${SERIALPORT} RDWR]
+	send "\r"
+	expect { "\r>" { } timeout { abort } }
+	send "pset ${OUTLET} 0\r"
+	expect { "pset ${OUTPUT} 0\r>" { } timeout { abort } }
+	EOF
+
+	expect -f ${EXPECTFILE} || exit 2
+
+	sleep ${OFFTIME}
+
+	cat <<- EOF > ${EXPECTFILE}
+	stty ospeed ${BAUDRATE} ispeed ${BAUDRATE} cs8 < ${SERIALPORT}
+	set timeout ${TIMEOUT}
+	spawn -open [open ${SERIALPORT} RDWR]
+	send "\r"
+	expect { "\r>" { } timeout { abort } }
+	send "pset ${OUTLET} 1\r"
+	expect { "pset ${OUTPUT} 1\r>" { } timeout { abort } }
+	EOF
+
+	expect -f ${EXPECTFILE} || exit 2
+
+	sleep ${ONTIME}
+
+	ITERATIONS=`expr ${ITERATIONS} - 1`
+
+done
+
+cat <<- EOF > ${EXPECTFILE}
 stty ospeed ${BAUDRATE} ispeed ${BAUDRATE} cs8 < ${SERIALPORT}
 set timeout ${TIMEOUT}
 spawn -open [open ${SERIALPORT} RDWR]
 send "\r"
 expect { "\r>" { } timeout { abort } }
-send "login\r"
-expect { "login\rUser ID: " { } timeout { abort } }
-send "${NETBOOTERLOGIN}\r"
-expect { "${NETBOOTERLOGIN}\rPassword:" { } timeout { abort } }
-send "${NETBOOTERPASSWD}\r"
-expect { "*****\r>" { } timeout { abort } }
-send "pset ${OUTLET} 0\r"
-expect { "pset ${OUTPUT} 0\r>" { } timeout { abort } }
-send "logout\r"
-expect { "logout\r>" { } timeout { abort } }
-EOF
-
-expect -f ${EXPECTFILE} || exit 2
-
-cat << EOF > ${EXPECTFILE}
-stty ospeed ${BAUDRATE} ispeed ${BAUDRATE} cs8 < ${SERIALPORT}
-set timeout ${TIMEOUT}
-spawn -open [open ${SERIALPORT} RDWR]
-send "\r"
-expect { "\r>" { } timeout { abort } }
-send "login\r"
-expect { "login\rUser ID: " { } timeout { abort } }
-send "${NETBOOTERLOGIN}\r"
-expect { "${NETBOOTERLOGIN}\rPassword:" { } timeout { abort } }
-send "${NETBOOTERPASSWD}\r"
-expect { "*****\r>" { } timeout { abort } }
-send "pset ${OUTLET} 1\r"
-expect { "pset ${OUTPUT} 1\r>" { } timeout { abort } }
 send "logout\r"
 expect { "logout\r>" { } timeout { abort } }
 EOF
