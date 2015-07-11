b87df9b0c14da089fa809da038ba1f2d1b8db26a
Chip Overclock <coverclock@diag.com>
2015-07-11 16:09:32 -0600

    Now actually cycling Raspberry Pi 2.

--- pogoswitch.sh
+++ pogoswitch.sh
@@ -14,9 +14,9 @@ TARGET_IPADDRESS="192.168.1.115"
 TARGET_LOGIN="pi"
 TARGET_PASSWD="raspberry"
 
+OFFTIME="30"
+ONTIME="120"
 ITERATIONS="10"
-OFFTIME="20"
-ONTIME="90"
 
 RC=0
 ZERO=`basename ${0}`
@@ -28,15 +28,15 @@ stty ospeed ${POWER_BAUDRATE} ispeed ${POWER_BAUDRATE} cs8 < ${POWER_SERIALPORT}
 set timeout ${POWER_TIMEOUT}
 spawn -open [open ${POWER_SERIALPORT} RDWR]
 send "\r"
-expect { "\r>" { } timeout { abort } }
+expect { "\r>" { } timeout { exit 2 } }
 send "login\r"
-expect { "login\rUser ID: " { } timeout { abort } }
+expect { "login\rUser ID: " { } timeout { exit 2 } }
 send "${POWER_LOGIN}\r"
-expect { "${POWER_LOGIN}\rPassword:" { } timeout { abort } }
+expect { "${POWER_LOGIN}\rPassword:" { } timeout { exit 2 } }
 send "${POWER_PASSWD}\r"
-expect { "*****\r>" { } timeout { abort } }
+expect { "*****\r>" { } timeout { exit 2 } }
 send "pset ${POWER_OUTLET} 1\r"
-expect { "pset ${POWER_OUTLET} 1\r>" { } timeout { abort } }
+expect { "pset ${POWER_OUTLET} 1\r>" { } timeout { exit 2 } }
 EOF
 
 expect -f ${EXPECTFILE} || exit 2
@@ -46,11 +46,11 @@ sleep ${ONTIME}
 cat <<- EOF > ${EXPECTFILE}
 set timeout ${TARGET_TIMEOUT}
 spawn ssh ${TARGET_LOGIN}@${TARGET_IPADDRESS}
-expect { "${TARGET_LOGIN}@${TARGET_IPADDRESS}'s password:" { } timeout { abort } }
+expect { "${TARGET_LOGIN}@${TARGET_IPADDRESS}'s password:" { } timeout { exit 2 } }
 send "${TARGET_PASSWD}\r"
-expect { "pi@raspberrypi ~ $ " { } timeout { abort } }
+expect { "pi@raspberrypi ~ $ " { } timeout { exit 2 } }
 send "exit \r"
-expect { "logout\rConnection to ${TARGET_IPADDRESS} closed.\r" { } timeout { abort } }
+expect { "logout\rConnection to ${TARGET_IPADDRESS} closed.\r" { } timeout { exit 2 } }
 EOF
     
 expect -f ${EXPECTFILE} || exit 2
@@ -62,9 +62,9 @@ while [ ${ITERATIONS} -gt 0 ]; do
 	set timeout ${POWER_TIMEOUT}
 	spawn -open [open ${POWER_SERIALPORT} RDWR]
 	send "\r"
-	expect { "\r>" { } timeout { abort } }
+	expect { "\r>" { } timeout { exit 2 } }
 	send "pset ${POWER_OUTLET} 0\r"
-	expect { "pset ${POWER_OUTLET} 0\r>" { } timeout { abort } }
+	expect { "pset ${POWER_OUTLET} 0\r>" { } timeout { exit 2 } }
 	EOF
 
 	expect -f ${EXPECTFILE} || exit 2
@@ -76,9 +76,9 @@ while [ ${ITERATIONS} -gt 0 ]; do
 	set timeout ${POWER_TIMEOUT}
 	spawn -open [open ${POWER_SERIALPORT} RDWR]
 	send "\r"
-	expect { "\r>" { } timeout { abort } }
+	expect { "\r>" { } timeout { exit 2 } }
 	send "pset ${POWER_OUTLET} 1\r"
-	expect { "pset ${POWER_OUTLET} 1\r>" { } timeout { abort } }
+	expect { "pset ${POWER_OUTLET} 1\r>" { } timeout { exit 2 } }
 	EOF
 
 	expect -f ${EXPECTFILE} || exit 2
@@ -88,11 +88,11 @@ while [ ${ITERATIONS} -gt 0 ]; do
 	cat <<- EOF > ${EXPECTFILE}
 	set timeout ${TARGET_TIMEOUT}
 	spawn ssh ${TARGET_LOGIN}@${TARGET_IPADDRESS}
-	expect { "${TARGET_LOGIN}@${TARGET_IPADDRESS}'s password:" { } timeout { abort } }
+	expect { "${TARGET_LOGIN}@${TARGET_IPADDRESS}'s password:" { } timeout { exit 2 } }
 	send "${TARGET_PASSWD}\r"
-	expect { "pi@raspberrypi ~ $ " { } timeout { abort } }
+	expect { "pi@raspberrypi ~ $ " { } timeout { exit 2 } }
 	send "exit \r"
-	expect { "logout\rConnection to ${TARGET_IPADDRESS} closed.\r" { } timeout { abort } }
+	expect { "logout\rConnection to ${TARGET_IPADDRESS} closed.\r" { } timeout { exit 2 } }
 	EOF
 	
 	expect -f ${EXPECTFILE} || exit 2
@@ -106,9 +106,9 @@ stty ospeed ${POWER_BAUDRATE} ispeed ${POWER_BAUDRATE} cs8 < ${POWER_SERIALPORT}
 set timeout ${POWER_TIMEOUT}
 spawn -open [open ${POWER_SERIALPORT} RDWR]
 send "\r"
-expect { "\r>" { } timeout { abort } }
+expect { "\r>" { } timeout { exit 2 } }
 send "logout\r"
-expect { "logout\r>" { } timeout { abort } }
+expect { "logout\r>" { } timeout { exit 2 } }
 EOF
 
 expect -f ${EXPECTFILE} || exit 2
