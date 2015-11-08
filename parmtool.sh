0824332686de51c8cd7c2276a34e74b782cd4d1f
Chip Overclock <coverclock@diag.com>
2015-11-07 21:50:33 -0700

    Checkpointing parmtool.

--- parmtool.sh
+++ parmtool.sh
@@ -9,17 +9,21 @@ ROOTETC=${PARMTOOLETC:-"${HOME}/.parmtool/db"}
 ROOTRUN=${PARMTOOLRUN:-"/tmp/${LOGNAME}/parmtool/db"}
 CAT=${PARMTOOLCAT:-"cat"}
 
-while getopts "hLlr:w:W:" OPT; do
+while getopts "hLld:r:w:W:X" OPT; do
 
 	case ${OPT} in
 
 	h)
-		echo "${ZERO} [ -L | -l | -r KEYWORD | -w KEYWORD=VALUE | -W KEYWORD=VALUE ]" 1>&2
+		echo "${ZERO} [ -L | -l | -X | -r KEYWORD | -d KEYWORD | -w KEYWORD=VALUE | -W KEYWORD=VALUE ]" 1>&2
 		;;
 
 	L)
-		test -d ${ROOTETC} && find ${ROOTETC} -type f -print -exec ${CAT} {} \;
-		test -d ${ROOTRUN} && find ${ROOTRUN} -type f -print -exec ${CAT} {} \;
+		if [ -d ${ROOTETC} ]; then
+			find ${ROOTETC} -type f -print
+		fi
+		if [ -d ${ROOTRUN} ]; then
+			find ${ROOTRUN} -type f -print
+		fi
 		;;
 
 	l)
@@ -27,10 +31,14 @@ while getopts "hLlr:w:W:" OPT; do
 		ROOTE="${ROOTETC}"
 		if [ -d ${FILER} -o -d ${FILEE} ]; then
 			TEMP0=`mktemp /tmp/${FILE}.XXXXXXXX`
-			test -d ${ROOTR} && ( cd ${ROOTR}; find . -type f -print ) >> ${TEMP0}
-			test -d ${ROOTE} && ( cd ${ROOTE}; find . -type f -print ) >> ${TEMP0}
+			if [ -d ${ROOTR} ]; then
+				( cd ${ROOTR}; find . -type f -print ) >> ${TEMP0}
+			fi
+			if [ -d ${ROOTE} ]; then
+				( cd ${ROOTE}; find . -type f -print ) >> ${TEMP0}
+			fi
 			TEMP1=`mktemp /tmp/${FILE}.XXXXXXXX`
-			sort < ${TEMP0} | uniq > ${TEMP1}
+			sort < ${TEMP0} | uniq | sed 's/^\.\///' > ${TEMP1}
 			rm -f ${TEMP0}
 			while read PATH1; do
 				FILER="${ROOTR}/${PATH1}"
@@ -56,6 +64,25 @@ while getopts "hLlr:w:W:" OPT; do
 		fi
 		;;
 
+	d)
+		KEYWORD=`echo "${OPTARG}" | awk -F '=' '{ print $1; }'`
+		PATHR="${KEYWORD//\.//}"
+		FILER="${ROOTRUN}/${PATHR}"
+		PATHE="${KEYWORD//\.//}"
+		FILEE="${ROOTETC}/${PATHE}"
+		if [ -f ${FILER} ]; then
+			rm -f ${FILER}
+		elif [ -d ${FILER} ]; then
+			rm -rf ${FILER}
+		elif [ -f ${FILEE} ]; then
+			rm -f ${FILEE}
+		elif [ -d ${FILEE} ]; then
+			rm -rf ${FILEE}
+		else
+			:
+		fi
+		;;
+
 	r)
 		KEYWORD=`echo "${OPTARG}" | awk -F '=' '{ print $1; }'`
 		PRE="${KEYWORD%\.\*}"
@@ -76,8 +103,22 @@ while getopts "hLlr:w:W:" OPT; do
 			done < ${FILE}
 		elif [ -d ${FILER} -o -d ${FILEE} ]; then
 			TEMP0=`mktemp /tmp/${FILE}.XXXXXXXX`
-			test -d ${ROOTR} && ( cd ${ROOTR}; test -d ${PATH0} && find ${PATH0} -type f -print ) >> ${TEMP0}
-			test -d ${ROOTE} && ( cd ${ROOTE}; test -d ${PATH0} && find ${PATH0} -type f -print ) >> ${TEMP0}
+			if [ -d ${ROOTR} ]; then
+				(
+					cd ${ROOTR}
+					if [ -d ${PATH0} ]; then
+						find ${PATH0} -type f -print
+					fi
+				) >> ${TEMP0}
+			fi
+			if [ -d ${ROOTE} ]; then
+				(
+					cd ${ROOTE}
+					if [ -d ${PATH0} ]; then
+						find ${PATH0} -type f -print
+					fi
+				) >> ${TEMP0}
+			fi
 			TEMP1=`mktemp /tmp/${FILE}.XXXXXXXX`
 			sort < ${TEMP0} | uniq > ${TEMP1}
 			rm -f ${TEMP0}
@@ -110,7 +151,9 @@ while getopts "hLlr:w:W:" OPT; do
 		PATHR="${KEYWORD//\.//}"
 		FILER="${ROOTRUN}/${PATHR}"
 		ROOTR=`dirname ${FILER}`
-		test -d ${ROOTR} || mkdir -p ${ROOTR}
+		if [ ! -d ${ROOTR} ]; then
+			 mkdir -p ${ROOTR}
+		fi
 		TEMPR=`mktemp ${FILER}.XXXXXXXX`
 		echo "${VALUE}" > ${TEMPR}
 		mv -f ${TEMPR} ${FILER}
@@ -121,10 +164,21 @@ while getopts "hLlr:w:W:" OPT; do
 		PATHE="${KEYWORD//\.//}"
 		FILEE="${ROOTETC}/${PATHE}"
 		ROOTE=`dirname ${FILEE}`
-		test -d ${ROOTE} || mkdir -p ${ROOTE}
+		if [ ! -d ${ROOTE} ]; then
+			mkdir -p ${ROOTE}
+		fi
 		TEMPE=`mktemp ${FILEE}.XXXXXXXX`
 		echo "${VALUE}" > ${TEMPE}
 		mv -f ${TEMPE} ${FILEE}
+		;;
+
+	X)
+		if [ -d ${ROOTETC} ]; then
+			find ${ROOTETC} -type f -print -exec ${CAT} {} \;
+		fi
+		if [ -d ${ROOTRUN} ]; then
+			find ${ROOTRUN} -type f -print -exec ${CAT} {} \;
+		fi
 		;;
 
 	esac
