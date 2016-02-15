1725e30a7a8056522c4356914f240413a78b5aaf
Chip Overclock <coverclock@diag.com>
2016-02-15 14:21:15 -0700

    Minor fixes to sleep functions from unit testing.

--- functions_sleep.sh
+++ functions_sleep.sh
@@ -4,8 +4,8 @@
 # Licensed under the GPLv2.
 
 function interruptible_sleep {
-	local DELAY=${1:-0}
-	sleep ${DELAY} &
+	local SLEEP=${1:-0}
+	sleep ${SLEEP} &
 	local PID=$!
 	wait ${PID}
 	kill ${PID} 2> /dev/null
@@ -13,15 +13,35 @@ function interruptible_sleep {
 
 function unjittered_sleep {
 	local THEN=${1}
-	local DELAY=${2:-0}
+	local SLEEP=${2:-0}
 	local MINIMUM=${3:-1}
 	local JITTER=0
 	local NOW=$(elapsedtime 1)
 	if [[ ${NOW} -gt ${THEN} ]]; then
 		JITTER=$(( ${NOW} - ${THEN} ))
 	fi
-	if [[ ${JITTER} -lt ${DELAY} ]]; then
-		MINIMUM=$(( ${DELAY} - ${JITTER} ))
+	if [[ ${JITTER} -lt ${SLEEP} ]]; then
+		MINIMUM=$(( ${SLEEP} - ${JITTER} ))
 	fi
 	interruptible_sleep ${MINIMUM}
+}
+
+function UNITTEST_1_interruptible_sleep {
+	while true; do
+		BEFORE=$(elapsedtime 1)
+		interruptible_sleep 10
+		AFTER=$(elapsedtime 1)
+		echo $((${AFTER} - ${BEFORE}))
+	done
+}
+
+function UNITTEST_2_unjittered_sleep {
+	JITTER=1
+	while true; do
+		EPOCH=$(elapsedtime  1)
+		date
+		sleep ${JITTER}
+		JITTER=$((4 - ${JITTER}))
+		unjittered_sleep ${EPOCH} 5
+	done
 }
