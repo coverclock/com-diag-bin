d7acda6c3db125f6d5ac24d6a9ff246d20b2299a
Chip Overclock <coverclock@diag.com>
2016-03-18 10:29:44 -0600

    [1] Fixed bug in critical section functions regarding handling flock fails. [2] Still testing on silver.

--- functions_critical_section.sh
+++ functions_critical_section.sh
@@ -5,51 +5,99 @@
 
 function critical_section_begin {
 	local FILE=${1}
-	local WAIT=${2:-10}
 	local LIST=($(ls /proc/self/fd))
 	local FD=$((${LIST[-1]}+1))
 	eval "exec ${FD}> ${FILE}"
-	flock -x -w ${WAIT} ${FD}
-	eval "echo ${FD} 1>&${FD}"
+	local WAIT=""
+	if [[ -n "${2}" ]]; then
+		WAIT="-w ${2}"
+	fi
+	if flock -x ${WAIT} ${FD}; then
+		eval "echo $$ 1>&${FD}"
+	else
+		eval "exec ${FD}>&-"
+		FD=""
+	fi
+	if [[ -z "${COM_DIAG_MUTEX_DEPTH}" ]]; then
+		COM_DIAG_MUTEX_DEPTH=0
+	fi
+	COM_DIAG_MUTEX_DEPTH=$((${COM_DIAG_MUTEX_DEPTH} + 1))
+	if [[ -n "${FD}" ]]; then
+		eval "COM_DIAG_MUTEX_FD_${COM_DIAG_MUTEX_DEPTH}=${FD}"
+		return 1
+	else
+		eval "COM_DIAG_MUTEX_FD_${COM_DIAG_MUTEX_DEPTH}="
+		return 0
+	fi
 }
 
 function critical_section_end {
-	local FILE=${1}
-	local FD
-	read FD < ${FILE}
-	flock -u ${FD}
-	eval "exec ${FD}>&-"
+	if [[ -n "${COM_DIAG_MUTEX_DEPTH}" ]]; then
+		eval "local FD=\$COM_DIAG_MUTEX_FD_${COM_DIAG_MUTEX_DEPTH}"
+		if [[ -n "${FD}" ]]; then
+			flock -u ${FD}
+			eval "exec ${FD}>&-"
+		fi
+		eval "COM_DIAG_MUTEX_FD_$$="
+		COM_DIAG_MUTEX_DEPTH=$((${COM_DIAG_MUTEX_DEPTH} - 1))
+	fi
+}
+
+function UNITTEST_0_critical_section {
+	MUTEX1=$(mktemp /tmp/$(basename $0).XXXXXXXXXX)
+	MUTEX2=$(mktemp /tmp/$(basename $0).XXXXXXXXXX)
+	echo A0
+	echo COM_DIAG_MUTEX_DEPTH=${COM_DIAG_MUTEX_DEPTH}
+	eval "echo COM_DIAG_MUTEX_FD_${COM_DIAG_MUTEX_DEPTH}=\$COM_DIAG_MUTEX_FD_${COM_DIAG_MUTEX_DEPTH}"
+	critical_section_begin ${MUTEX1}
+		echo B1
+		echo COM_DIAG_MUTEX_DEPTH=${COM_DIAG_MUTEX_DEPTH}
+		eval "echo COM_DIAG_MUTEX_FD_${COM_DIAG_MUTEX_DEPTH}=\$COM_DIAG_MUTEX_FD_${COM_DIAG_MUTEX_DEPTH}"
+		critical_section_begin ${MUTEX2}
+			echo C2
+			echo COM_DIAG_MUTEX_DEPTH=${COM_DIAG_MUTEX_DEPTH}
+			eval "echo COM_DIAG_MUTEX_FD_${COM_DIAG_MUTEX_DEPTH}=\$COM_DIAG_MUTEX_FD_${COM_DIAG_MUTEX_DEPTH}"
+		critical_section_end
+		echo B3
+		echo COM_DIAG_MUTEX_DEPTH=${COM_DIAG_MUTEX_DEPTH}
+		eval "echo COM_DIAG_MUTEX_FD_${COM_DIAG_MUTEX_DEPTH}=\$COM_DIAG_MUTEX_FD_${COM_DIAG_MUTEX_DEPTH}"
+	critical_section_end
+	echo A4
+	echo COM_DIAG_MUTEX_DEPTH=${COM_DIAG_MUTEX_DEPTH}
+	eval "echo COM_DIAG_MUTEX_FD_${COM_DIAG_MUTEX_DEPTH}=\$COM_DIAG_MUTEX_FD_${COM_DIAG_MUTEX_DEPTH}"
+	rm -f ${MUTEX2}
+	rm -f ${MUTEX1}
 }
 
 function UNITTEST_1_critical_section {
-	LOCKFILE=$(mktemp /tmp/$(basename $0).XXXXXXXXXX)
+	MUTEX=$(mktemp /tmp/$(basename $0).XXXXXXXXXX)
 	ls /proc/self/fd 1>&2
-	critical_section_begin ${LOCKFILE}
+	critical_section_begin ${MUTEX}
+		ls /proc/self/fd 1>&2
+	critical_section_end
 	ls /proc/self/fd 1>&2
-	critical_section_end ${LOCKFILE}
-	ls /proc/self/fd 1>&2
-	rm ${LOCKFILE}
+	rm ${MUTEX}
 }
 
 function UNITTEST_2_critical_section {
-	LOCKFILE=$(mktemp /tmp/$(basename $0).XXXXXXXXXX)
+	MUTEX=$(mktemp /tmp/$(basename $0).XXXXXXXXXX)
 	(
 		echo "A1"
-		critical_section_begin ${LOCKFILE}
+		critical_section_begin ${MUTEX}
 			echo "A2"
 			sleep 5
 			echo "A3"
-		critical_section_end ${LOCKFILE}
+		critical_section_end
 		echo "A4"
 	) &
 	A=$!
 	(
 		echo "B1"
-		critical_section_begin ${LOCKFILE}
+		critical_section_begin ${MUTEX}
 			echo "B2"
 			sleep 5
 			echo "B3"
-		critical_section_end ${LOCKFILE}
+		critical_section_end
 		echo "B4"
 	) &
 	B=$!
@@ -57,5 +105,5 @@ function UNITTEST_2_critical_section {
 	echo "B0"
 	wait ${A}
 	echo "A0"
-	rm ${LOCKFILE}
+	rm ${MUTEX}
 }
