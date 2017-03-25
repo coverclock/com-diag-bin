232a2c30e201dd444175bb73a4a4e1386820ad46
Chip Coverclock <coverclock@diag.com>
2017-03-25 15:47:03 -0600

    [1] Fixed scripts to also work with LeoNTP. [2] Tested on both Hourclass and Sundial (LeoNTP).

--- clocktime.sh
+++ clocktime.sh
@@ -2,10 +2,8 @@
 # Extract the local clock time from the NTP server host.
 SERVER=${1:-"hourglass"}
 REFTIME=$(ntpq -c "rv 0 clock" ${SERVER})
-# clock=dc73db64.0e3e1f16  Wed, Mar 15 2017  9:25:24.055
-STRTIME=${REFTIME#clock=* }
+STRTIME=$(echo ${REFTIME} | awk '/clock=/ { sub(/,/,"",$6); print $2,$3,$4,$5,$6; }')
 ISOTIME=$(date -d "${STRTIME}" +"%Y-%m-%d %H:%M:%S.%N %Z")
-# 2017-03-15 09:33:56.055000000 MDT
 DSPTIME="${ISOTIME:0:23} ${ISOTIME:30}"
 echo ${DSPTIME}
 
--- referencetime.sh
+++ referencetime.sh
@@ -2,9 +2,7 @@
 # Extract the NTP reference time - when the clock was last updated -from the NTP server.
 SERVER=${1:-"hourglass"}
 REFTIME=$(ntpq -c "rv 0 reftime" ${SERVER})
-# reftime=dc73db64.0e3e1f16  Wed, Mar 15 2017  9:25:24.055
-STRTIME=${REFTIME#reftime=* }
+STRTIME=$(echo ${REFTIME} | awk '/reftime=/ { sub(/,/,"",$6); print $2,$3,$4,$5,$6; }')
 ISOTIME=$(date -d "${STRTIME}" +"%Y-%m-%d %H:%M:%S.%N %Z")
-# 2017-03-15 09:33:56.055000000 MDT
 DSPTIME="${ISOTIME:0:23} ${ISOTIME:30}"
 echo ${DSPTIME}
