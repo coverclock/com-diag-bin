39a590f446cebc78cfb1d7dfc3e99b52735c5b1e
Chip Overclock <coverclock@diag.com>
2017-04-02 10:55:24 -0600

    [1] Added comments on meaning of characters before NTPD server names. [2] Tested.

--- networktime.sh
+++ networktime.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 # Fetch some NTP info from the time server.
 SERVER=${1:-"localhost"}
+# \b	reject
+# x	falsetick
+# .	excess
+# -	outlyer
+# +	candidate
+# #	selected (but not amongst top six)
+# *	peer
+# .	PPS peer
 exec ntpq -c peer -c as -c rl ${SERVER}
