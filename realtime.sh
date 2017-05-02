3088c7ea36a3a17ba31c8f68556fa7bb43b98b96
Chip Coverclock <coverclock@diag.com>
2017-05-02 09:41:13 -0600

    [1] realtime.sh now conforms to other scripts.

--- realtime.sh
+++ realtime.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 # Extract the time from the real-time hardware clock. 
-SERVER=${1:-"pi@hourglass"}
-HRDTIME=$(ssh ${SERVER} sudo /sbin/hwclock -u -r)
+SERVER=${1:-"localhost"}
+if [[ "${SERVER}" == "localhost" ]]; then
+	HRDTIME=$(sudo /sbin/hwclock -u -r)
+else
+	HRDTIME=$(ssh ${SERVER} sudo /sbin/hwclock -u -r)
+fi
 # Sun 19 Mar 2017 02:39:37 PM MDT  -0.891855 seconds
 FLDTIME=${HRDTIME//:/ }
 # Sun 19 Mar 2017 02 39 37 PM MDT  -0.891855 seconds
 set ${FLDTIME}
 HH=${5}
-if [[ ${8} == "PM" ]]; then
+if [[ "${8}" == "PM" ]]; then
 	HH=$((${HH} + 12))
 fi
 STRTIME="${1} ${3} ${2} ${HH}:${6}:${7} ${9} ${4}"
