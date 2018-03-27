c67547b4cbbf5d53b0ef429e405995ee4a121ea8
Chip Overclock <coverclock@diag.com>
2018-03-27 09:39:17 -0600

    [1] Fixed bug in script. [2] Untested.

--- installpiimage.sh
+++ installpiimage.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Copyright 2018 Digital Aggregates Corporation, Arvada CO USA.
-IMG=${1-"/dev/null"}
-DEV=${2-"/dev/null"}
-CMD="dd bs=4M if=${IMG} of=${DEV} conv=fsync"
+IMG=${1:-"/dev/null"}
+DEV=${2:-"/dev/null"}
+BLK=${3:-"4M"}
+CMD="dd bs=${BLK} if=${IMG} of=${DEV} conv=fsync"
 echo "sudo ${CMD}" 1>&2
 echo -n "[yN]? "
 read YN
