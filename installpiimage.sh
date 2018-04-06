1ee7f8dcfce51345d94a6ce86d66ccfff72ed45d
Chip Overclock <coverclock@diag.com>
2018-04-06 10:39:18 -0600

    [1] Updated Eclipse files. [2] Added check for files in script.

--- installpiimage.sh
+++ installpiimage.sh
@@ -5,6 +5,8 @@ IMG=${1:-"/dev/null"}
 DEV=${2:-"/dev/null"}
 BLK=${3:-"4M"}
 CMD="dd bs=${BLK} if=${IMG} of=${DEV} conv=fsync"
+test -f ${IMG} || exit 2
+test -b ${DEV} || exit 2
 echo "sudo ${CMD}" 1>&2
 echo -n "[yN]? "
 read YN
