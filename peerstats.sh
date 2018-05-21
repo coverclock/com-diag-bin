0b591de0ba456677e53142e9a4da9417ec5528b7
Chip Overclock <coverclock@diag.com>
2018-05-21 14:32:53 -0600

    [1] Updated to Candleclock's peerstats.

--- peerstats.sh
+++ peerstats.sh
@@ -3,9 +3,10 @@
 # Compute the average offset for the GPS (0) and PPS (1) reference clock.
 # <http://catb.org/gpsd/gpsd-time-service-howto.html>
 # <https://docs.ntpsec.org/latest/ntp_conf.html>
-FILE=${1-"/var/log/ntpstats/peerstats"}
-exec awk '
-     /SHM\(0\)/ { tot0 += $5; num0++; avg0 = tot0 / num0; print $5, tot0, num0, avg0; }
-     /SHM\(1\)/ { tot1 += $5; num1++; avg1 = tot1 / num1; print $5, tot1, num1, avg1; }
-     END { print "SHM(0) time1", 0 - avg0; print "SHM(1) time1", 0 - avg1; }
-' < ${FILE}
+ROOT=${1-"/var/log/ntpstats/peerstats"}
+cat ${ROOT}.* | awk '
+    BEGIN { tot0 = 0; num0 = 0; tot1 = 0; num1 = 0; }
+    $3 == "SHM(0)" { tot0 += $5; num0++; avg0 = tot0 / num0; print $3, $5, tot0, num0, avg0; }
+    $3 == "SHM(1)" { tot1 += $5; num1++; avg1 = tot1 / num1; print $3, $5, tot1, num1, avg1; }
+    END { print "SHM(0) time1", 0 - avg0; print "SHM(1) time1", 0 - avg1; }
+'
