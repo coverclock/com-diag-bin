6c72c1a1ad0c958c3265d164757288e2dc890523
Chip Overclock <coverclock@diag.com>
2018-05-30 09:32:20 -0600

    [1] Added script to extract Pi image from SD card. [2] Tested.

---
+++ extractpiimage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Copyright 2018 Digital Aggregates Corporation, Arvada CO USA.
+# usage: extractpiimage.sh /dev/sdc ./framistat.bin 1M
+DEV=${1:-"/dev/null"}
+IMG=${2:-"/dev/null"}
+BLK=${3:-"4M"}
+CMD="dd bs=${BLK} if=${DEV} of=${IMG}"
+test -f ${IMG} && exit 2
+test -b ${DEV} || exit 2
+echo "sudo ${CMD}" 1>&2
+echo -n "[yN]? "
+read YN
+case "${YN}" in
+[Yy]) ;;
+*) exit 1;;
+esac
+exec sudo ${CMD}
