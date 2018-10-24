59347cf98744b45977c82fbe158a8c296c0b9ba9
Chip Overclock <coverclock@diag.com>
2018-10-24 13:10:11 -0600

    [1] Updated to latest eclipse path.

--- eclipse.sh
+++ eclipse.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #eclipse "${@}" -vmargs -Xms512m -Xmx4096m
-${HOME}/projects/eclipse/cpp-oxygen/eclipse/eclipse "${@}"
+#${HOME}/projects/eclipse/cpp-oxygen/eclipse/eclipse "${@}"
+${HOME}/projects/eclipse/cpp-simrel/eclipse/eclipse "${@}"
