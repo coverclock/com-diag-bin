9aa4a2dac37e777ba61a9731dd6a65343c5915af
Chip Overclock <coverclock@diag.com>
2018-11-28 09:38:38 -0700

    [1] Took safeties off detab.

--- detab.sh
+++ detab.sh
@@ -2,6 +2,7 @@
 # vi: set ts=4:
 # Copyright 2018 Digital Aggregates Corporation.
 # Licensed under the terms in LICENSE.txt.
+# usage: $0 [ -t TAB ] FILE [ ... ]
 
 ZERO=$(basename ${0})
 TEMP=$(mktemp ${TMPDIR:="/tmp"}/${ZERO}.XXXXXXXXXX)
@@ -9,15 +10,12 @@ TABS=4
 
 if [ "$1" = "-t" ]; then
 	shift
-	TABS=$2
+	TABS=$1
 	shift
 fi
 
 for FILE in $*; do
-	echo ${FILE} ${TABS}
-	if true; then
-		:
-	elif expand -i -t ${TABS} < ${FILE} > ${TEMP}; then
+	if expand -i -t ${TABS} < ${FILE} > ${TEMP}; then
 		mv ${TEMP} ${FILE}
 	else
 		exit 1
