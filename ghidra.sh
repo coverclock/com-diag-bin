1365aa6b483c0d9e8a1589c5f7d7a0bd59c6920f
Chip Overclock <coverclock@diag.com>
2019-10-21 11:24:07 -0600

    [1] Added listinstalled script. [2] Updated ghidra script to point to later version.

--- ghidra.sh
+++ ghidra.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Copyright 2019 by the Digital Aggregates Corporation, Colorado, USA.
 export PATH="${HOME}/projects/jdk-11.0.2/bin:${PATH}"
-exec ${HOME}/projects/ghidra_9.0/ghidraRun
+exec ${HOME}/projects/ghidra_9.0.2/ghidraRun
