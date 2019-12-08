8d33320e0529f56f496d2d972e370437e2b99267
Chip Overclock <coverclock@diag.com>
2019-12-07 17:52:42 -0700

    [1] Backup Pi image script now seperates dd from zip. (TESTED)

--- backuppiimage.sh
+++ backuppiimage.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 # Copyright 2019 Digital Aggregates Corporation, Arvada CO USA.
-# usage: backuppiimage.sh /dev/sdc /mnt/framistat.zip
+# usage: backuppiimage.sh /dev/sdc /mnt/framistat.zip 4M
+# N.B.: Decompressing the zipped image consistently throws an
+# "invalid compressed data--length error" message seemingly no
+# matter what I try. But the resulting decompressed file diffs
+# just fine against the original image.
 DEV=${1:-"/dev/null"}
 ZIP=${2:-"/dev/null"}
-CMD="sudo dd if=${DEV} | zip ${ZIP} -"
+BLK=${3:-"4M"}
+TMP=$(mktemp ${TMPDIR:="/tmp"}/$(basename $0).XXXXXXXXXX)
+trap "rm -f ${TMP}" HUP INT TERM EXIT
+CMD="dd bs=${BLK} if=${DEV} of=${TMP} && zip ${ZIP} ${TMP}"
 test -f ${ZIP} && exit 2
 test -b ${DEV} || exit 2
 echo "${CMD}" 1>&2
