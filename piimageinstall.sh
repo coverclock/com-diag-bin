00c20d14db7efd0c5c3039a13454dcdc8dadede5
Chip Overclock <coverclock@diag.com>
2020-01-06 10:02:23 -0700

    [1] Changing install script to use zip file. UNTESTED

--- piimageinstall.sh
+++ piimageinstall.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-# Copyright 2018-2019 Digital Aggregates Corporation, Arvada CO USA.
-# USAGE piimageinstall.sh IMGFILE DEVICE [ BLOCKSIZE ]
-# EXAMPLE piimageinstall.sh rpi3.img /dev/sdb
-IMG=${1:-"/dev/null"}
+# Copyright 2018-2020 Digital Aggregates Corporation, Arvada CO USA.
+# USAGE piimageinstall.sh ZIPFILE DEVICE [ BLOCKSIZE ]
+# EXAMPLE piimageinstall.sh rpi3.zip /dev/sdb
+ZIP=${1:-"/dev/null"}
 DEV=${2:-"/dev/null"}
 BLK=${3:-"4M"}
-CMD="dd bs=${BLK} if=${IMG} of=${DEV} conv=fsync"
-test -f ${IMG} || exit 2
+CMD="unzip ${ZIP} | sudo dd bs=${BLK} of=${DEV} conv=fsync"
+test -f ${ZIP} || exit 2
 test -b ${DEV} || exit 2
-echo "sudo ${CMD}" 1>&2
+echo "${CMD}" 1>&2
 echo -n "[yN]? "
 read YN
 case "${YN}" in
 [Yy]) ;;
 *) exit 1;;
 esac
-exec sudo ${CMD}
+eval ${CMD}
