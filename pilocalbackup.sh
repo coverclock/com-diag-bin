f0e78911e10c2b77ce85e1aeca9bd16d8178f803
Chip Overclock <coverclock@diag.com>
2021-03-11 13:37:35 -0700

    [1] Added script to backup Tesoro data drive. UNTESTED [2] Typo.

--- pilocalbackup.sh
+++ pilocalbackup.sh
@@ -63,7 +63,7 @@ sudo rsync -axHv --delete-during                       ${BOT} ${ONE} 1>&2 || RC=
 sudo rsync -axHv --delete-during --exclude-from=${EXC} ${ROT} ${TWO} 1>&2 || RC=4
 sudo rsync -axHv                                       ${INC} ${DEV} 1>&2 || RC=5
 
-# THis can take a long time, like tens of minutes. You
+# This can take a long time, like tens of minutes. You
 # can either take this flash write latency here, or when
 # you unmount the backup device. I chose to embed it in
 # the script, rather than leave the surprise for later.
