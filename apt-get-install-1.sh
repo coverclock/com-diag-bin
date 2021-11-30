8576d6bed32c7b873e93277274daa42f9ead2993
jsloan <jsloan@diag.com>
2021-11-30 08:35:31 -0700

    [1] Further refactoring of apt-get install scripts.

--- apt-get-install-1.sh
+++ apt-get-install-1.sh
@@ -6,7 +6,6 @@ apt-get --yes install grip
 apt-get --yes install inotify-tools
 apt-get --yes install make
 apt-get --yes install net-tools
-apt-get --yes install openssh-server
 apt-get --yes install screen
 apt-get --yes install socat
 apt-get --yes install vim
