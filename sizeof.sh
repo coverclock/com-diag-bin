cf884a072d69a5ae788c0ccd538eaf385b7ef279
Chip Overclock <coverclock@diag.com>
2022-10-27 07:29:36 -0600

    [1] Added sizeof convenience script. [2] Comments.

--- sizeof.sh
+++ sizeof.sh
@@ -2,9 +2,9 @@
 # Copyright 2022 Digital Aggregates Corporation, Arvada Colorado USA.
 # https://github.com/coverclock/com-diag-hazer
 # mailto:coverclock@diag.com
-# Convenience script to run vintage remotely via ssh.
+# Convenience script to run sizeof remotely via ssh.
 ROOT=${HOME}/src/com-diag-diminuto/Diminuto
 cd ${ROOT}
 EXEC=out/host/bin
 . ${EXEC}/setup
-exec ${EXEC}/vintage
+exec ${EXEC}/sizeof
