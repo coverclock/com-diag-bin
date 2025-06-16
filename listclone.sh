9950cc285aea7ff0b5b2d73613ef8f2fe3b86673
Chip Overclock <coverclock@diag.com>
2025-06-16 10:55:21 -0600

    [1] Okay, so THIS is the first cut at the clone script.

---
+++ listclone.sh
@@ -0,0 +1,7 @@
+git clone \
+https://github.com/coverclock/com-cranequin-overlay \
+https://github.com/coverclock/com-diag-bin \
+https://github.com/coverclock/com-diag-codex \
+https://github.com/coverclock/com-diag-diminuto \
+https://github.com/coverclock/com-diag-hazer \
+https://github.com/coverclock/com-diag-placer
